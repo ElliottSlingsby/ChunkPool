b86e94db6bb6a0eb80a6a625d3ad4029ce36f56a
Elliott Slingsby <elliottslingsby@googlemail.com>
2016-10-01 22:45:50 +0100

    Getting and setting blocks

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,83 @@ int main(int argc, char *argv[]){
 	srand((unsigned int)time(nullptr));
 
 	ObjectPool pool(CHUNK);
+
+	const char testStr0[] = "Fusce ac turpis aliquet, tristique diam id, venenatis nisl.";
+	const char testStr1[] = "Integer congue tortor euismod tristique elementum.";
+	const char testStr2[] = "Ut eget nulla vulputate, vestibulum neque.";
+	const char testStr3[] = "Sed fringilla.";
+	const char testStr4[] = "Praesent gravida risus eu orci viverra, in eleifend magna aliquam.";
+	const char testStr5[] = "Donec egestas diam et libero accumsan, eu dictum leo faucibus.";
+	const char testStr6[] = "Pellentesque id massa eu nulla pretium.";
+	const char testStr7[] = "Quisque suscipit ex ac.";
+
+	uint64_t index0 = pool.insert(sizeof(testStr0));
+	uint64_t index1 = pool.insert(sizeof(testStr1));
+	uint64_t index2 = pool.insert(sizeof(testStr2));
+	uint64_t index3 = pool.insert(sizeof(testStr3));
+	uint64_t index4 = pool.insert(sizeof(testStr4));
+	uint64_t index5 = pool.insert(sizeof(testStr5));
+	uint64_t index6 = pool.insert(sizeof(testStr6));
+	uint64_t index7 = pool.insert(sizeof(testStr7));
+
+	std::memcpy(pool.get(index0), testStr0, sizeof(testStr0));
+	std::memcpy(pool.get(index1), testStr1, sizeof(testStr1));
+	std::memcpy(pool.get(index2), testStr2, sizeof(testStr2));
+	std::memcpy(pool.get(index3), testStr3, sizeof(testStr3));
+	std::memcpy(pool.get(index4), testStr4, sizeof(testStr4));
+	std::memcpy(pool.get(index5), testStr5, sizeof(testStr5));
+	std::memcpy(pool.get(index6), testStr6, sizeof(testStr6));
+	std::memcpy(pool.get(index7), testStr7, sizeof(testStr7));
+
+
+	pool.remove(index0);
+	pool.remove(index1);
+	pool.remove(index3);
+	pool.remove(index6);
+	pool.remove(index5);
+
+	pool.processRemoved();
+
+	index0 = pool.insert(sizeof(testStr0));
+	index1 = pool.insert(sizeof(testStr1));
+	index3 = pool.insert(sizeof(testStr3));
+	index5 = pool.insert(sizeof(testStr5));
+	index6 = pool.insert(sizeof(testStr6));
+
+	//std::memcpy(pool.get(index0), testStr0, sizeof(testStr0));
+	//std::memcpy(pool.get(index1), testStr1, sizeof(testStr1));
+	//std::memcpy(pool.get(index3), testStr3, sizeof(testStr3));
+	//std::memcpy(pool.get(index5), testStr5, sizeof(testStr5));
+	//std::memcpy(pool.get(index6), testStr6, sizeof(testStr6));
+
+	
+	char* content0 = (char*)pool.get(index0);
+	char* content1 = (char*)pool.get(index1);
+	char* content2 = (char*)pool.get(index2);
+	char* content3 = (char*)pool.get(index3);
+	char* content4 = (char*)pool.get(index4);
+	char* content5 = (char*)pool.get(index5);
+	char* content6 = (char*)pool.get(index6);
+	char* content7 = (char*)pool.get(index7);
+
+	if (content0)
+		printf("Equal = %d %s\n", std::strcmp(testStr0, content0) == 0, content0);
+	if (content1)
+		printf("Equal = %d %s\n", std::strcmp(testStr1, content1) == 0, content1);
+	if (content2)
+		printf("Equal = %d %s\n", std::strcmp(testStr2, content2) == 0, content2);
+	if (content3)
+		printf("Equal = %d %s\n", std::strcmp(testStr3, content3) == 0, content3);
+	if (content4)
+		printf("Equal = %d %s\n", std::strcmp(testStr4, content4) == 0, content4);
+	if (content5)
+		printf("Equal = %d %s\n", std::strcmp(testStr5, content5) == 0, content5);
+	if (content6)
+		printf("Equal = %d %s\n", std::strcmp(testStr6, content6) == 0, content6);
+	if (content7)
+		printf("Equal = %d %s\n", std::strcmp(testStr7, content7) == 0, content7);
+
+	
 	std::list<uint64_t> ids;
 
 	for (unsigned int n = 0; n < LOOPS; n++){
--- ObjectPool.hpp
+++ ObjectPool.hpp
@@ -7,13 +7,13 @@
 #include <stack>
 
 class ObjectPool{
+protected:
 	uint8_t* _buffer = nullptr;
 	uint64_t _bufferSize = 0;
 
 	uint64_t _chunkSize = 0;
 
-	std::vector<MemHelp::Location> _indexLocations;
-	std::vector<uint64_t> _indexMasks;
+	std::vector<MemHelp::Info> _indexLocations;
 	std::vector<uint8_t> _indexRemoved;
 
 	std::vector<MemHelp::Info> _freeMemory;
@@ -36,13 +36,19 @@ class ObjectPool{
 
 public:
 	ObjectPool(uint64_t chunkSize);
+	virtual ~ObjectPool();
 
-	template <typename ...Ts>
 	inline uint64_t insert(uint64_t size);
+
+	inline uint8_t* get(uint64_t index);
 
 	inline void remove(uint64_t index);
 
+	inline void reserve(uint64_t size);
+
 	inline void processRemoved(uint64_t limit = 0);
+
+	inline void clear();
 
 	inline void setChunkSize(uint64_t size);
 
@@ -75,12 +81,11 @@ inline void ObjectPool::_shrinkBuffer(){
 
 inline void ObjectPool::_expandInfoBuffers(){
 	_indexLocations.push_back(0);
-	_indexMasks.push_back(0);
 	_indexRemoved.push_back(0);
 }
 
 inline bool ObjectPool::_validIndex(uint64_t index) const{
-	if (index < _indexLocations.size() && _indexLocations[index] != 0)
+	if (index < _indexLocations.size() && _indexLocations[index] != 0 && _indexRemoved[index] == 0)
 		return true;
 
 	return false;
@@ -127,9 +132,7 @@ inline void ObjectPool::_returnMemory(MemHelp::Info memory, bool rebuildQueue){
 }
 
 inline MemHelp::Info ObjectPool::_findMemory(uint64_t size){
-	while (_freeMemoryQueue.empty() || _freeMemoryQueue.top().size < size){
-		_expandBuffer();
-	}
+	reserve(size);
 
 	std::stack<MemHelp::Info> larger;
 
@@ -164,7 +167,10 @@ inline ObjectPool::ObjectPool(uint64_t chunkSize){
 	_chunkSize = chunkSize;
 }
 
-template<typename ...Ts>
+inline ObjectPool::~ObjectPool(){
+	std::free(_buffer);
+}
+
 inline uint64_t ObjectPool::insert(uint64_t size){
 	uint64_t index = 0;
 
@@ -181,11 +187,19 @@ inline uint64_t ObjectPool::insert(uint64_t size){
 	MemHelp::Info memory = _findMemory(size);
 
 	_indexLocations[index] = memory;
-	_indexMasks[index] = UINT64_MAX;
 
 	std::memset(_buffer + memory.start, 0, memory.size);
 
 	return index;
+}
+
+inline uint8_t* ObjectPool::get(uint64_t index){
+	if (!_validIndex(index))
+		return nullptr;
+
+	MemHelp::Info memory = _indexLocations[index];
+
+	return _buffer + memory.start;
 }
 
 inline void ObjectPool::remove(uint64_t index){
@@ -194,6 +208,12 @@ inline void ObjectPool::remove(uint64_t index){
 
 	_indexRemoved[index] = 1;
 	_removedIndexes.push(index);
+}
+
+inline void ObjectPool::reserve(uint64_t size){
+	while (_freeMemoryQueue.empty() || _freeMemoryQueue.top().size < size){
+		_expandBuffer();
+	}
 }
 
 inline void ObjectPool::processRemoved(uint64_t limit){
@@ -208,7 +228,6 @@ inline void ObjectPool::processRemoved(uint64_t limit){
 		_returnMemory(_indexLocations[index], false);
 
 		_indexLocations[index] = 0;
-		_indexMasks[index] = 0;
 		_indexRemoved[index] = 0;
 
 		_removedIndexes.pop();
@@ -218,6 +237,20 @@ inline void ObjectPool::processRemoved(uint64_t limit){
 	_rebuildFreeMemoryQueue();
 
 	_shrinkBuffer();
+}
+
+inline void ObjectPool::clear(){
+	std::free(_buffer);
+	_buffer = nullptr;
+
+	_bufferSize = 0;
+
+	std::vector<MemHelp::Info>().swap(_indexLocations);
+	std::vector<uint8_t>().swap(_indexRemoved);
+	std::vector<MemHelp::Info>().swap(_freeMemory);
+
+	_freeMemoryQueue = std::priority_queue<MemHelp::Size>();
+	_removedIndexes = std::queue<uint64_t>();
 }
 
 inline void ObjectPool::setChunkSize(uint64_t size){
